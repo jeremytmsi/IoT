6b243de9858e77bb6b3ddbe4c6f43263aa65f199
Jeremy Tomasi <me@jeremytomasi.fr>
2025-04-11 08:42:44 +0200

    jsonnet compile

--- main.tf
+++ main.tf
@@ -4,6 +4,10 @@ terraform {
       source  = "grafana/grafana"
       version = "~> 2.0"
     }
+
+    jsonnet = {
+      source = "alxrem/jsonnet"
+    }
   }
 }
 
@@ -13,7 +17,15 @@ provider "grafana" {
   auth = "${var.grafana_api_token}"
 }
 
+provider "jsonnet" {
+  jsonnet_path = join(":", ["${path.module}/jsonnet", "${path.module}/jsonnet/grafonnet-lib", "${path.module}/vendor"])
+}
+
+data "jsonnet_file" "dashboard" {
+    source = "${path.module}/dashboards/grafana.jsonnet"
+}
+
 # On importe le dashboard dans Grafana
 resource "grafana_dashboard" "grafana" {
-  config_json = file("${path.module}/dashboards/grafana.json")
+  config_json = data.jsonnet_file.dashboard.rendered
 }
